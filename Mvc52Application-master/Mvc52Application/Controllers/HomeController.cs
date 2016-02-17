4b576c4b3929457c57a25e1cde0b0b012f0f8943
sandhya124 <gita.azure@gmail.com>
2016-02-17 22:35:29 +0530

    ok

---
+++ Mvc52Application-master/Mvc52Application/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc52Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Trace.TraceInformation("{0}: This is an informational trace message", DateTime.Now);
+            Trace.TraceWarning("{0}: Here is trace warning", DateTime.Now);
+            Trace.TraceError("{0}: Something is broken; tracing an error!", DateTime.Now);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            string foo = ConfigurationManager.AppSettings["foo"];
+            ViewBag.Message = String.Format("The value of setting foo is: '{0}'", foo);
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
